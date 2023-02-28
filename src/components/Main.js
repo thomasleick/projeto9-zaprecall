d0d84d7dca898011208da62e976a1921251f89cb
Thomas Andrioli Leick <thomasaleick@gmail.com>
2023-02-28 04:03:43 -0300

    doing MainDiv

--- src/components/Main.js
+++ src/components/Main.js
@@ -2,7 +2,8 @@ import React from 'react';
 import styled from 'styled-components';
 
 const MainDiv = styled.main`
-    
+    min-height: 477px;
+    background-color: #FB6B6B;
 `
 
 const Main = () => {
