32b97cf054bdfdcc6d37bdb153aa446010d6511b
Thomas Andrioli Leick <thomasaleick@gmail.com>
2023-03-01 04:31:41 -0300

    animated Welcome Component

--- src/components/App.js
+++ src/components/App.js
@@ -1,49 +1,107 @@
-import Header from "./Header"
-import Main from "./Main"
-import Footer from "./Footer"
-import styled from "styled-components"
-import decks from '../decks';
+import Header from "./Header";
+import Main from "./Main";
+import Footer from "./Footer";
+import styled, { keyframes } from "styled-components";
+import decks from "../decks";
 import { useState } from "react";
 import Welcome from "./Welcome";
-import { Transition } from 'react-transition-group';
+import { CSSTransition } from "react-transition-group";
 
 const AppDiv = styled.div`
   display: flex;
   flex-direction: column;
   justify-content: space-between;
   align-items: center;
-  font-family: 'Recursive', sans-serif, 'Righteous', cursive;
-`
+  font-family: "Recursive", sans-serif, "Righteous", cursive;
+`;
 
 const ViewPort = styled.div`
-  border: 1px solid #DBDBDB;
+  border: 1px solid #dbdbdb;
   width: 375px;
   height: 100vh;
 
-  @media(max-width: 375px) {
+  @media (max-width: 375px) {
     width: 100vw;
   }
-`
+`;
+
+const fadeIn = keyframes`
+  from {
+    opacity: 0;
+    transform: translateX(-50%);
+  }
+  to {
+    opacity: 1;
+    transform: translateX(0);
+  }
+`;
+
+const fadeOut = keyframes`
+  from {
+    opacity: 1;
+    transform: translateX(0);
+  }
+  to {
+    opacity: 0;
+    transform: translateX(50%);
+  }
+`;
+
+const AnimatedWelcome = styled.div`
+  width: 100%;
+  height: 100%;
+  opacity: 0;
+  animation: ${fadeIn} 0.5s ease-in-out forwards;
+
+  &.fade-enter {
+    opacity: 0;
+    transform: translateX(-50%);
+  }
+
+  &.fade-enter-active {
+    opacity: 1;
+    transform: translateX(0);
+    animation: ${fadeIn} 0.5s ease-in-out forwards;
+  }
+
+  &.fade-exit {
+    opacity: 1;
+    transform: translateX(0);
+  }
+
+  &.fade-exit-active {
+    opacity: 0;
+    transform: translateX(50%);
+    animation: ${fadeOut} 0.5s ease-in-out forwards;
+  }
+`;
 
 const App = () => {
-
-  const [isStarted, setIsStarted] = useState(false)
+  const [isStarted, setIsStarted] = useState(false);
 
   return (
     <AppDiv>
       <ViewPort>
-        {isStarted ? 
+        <CSSTransition
+          in={!isStarted}
+          classNames="fade"
+          timeout={500}
+          unmountOnExit
+        >
+          <AnimatedWelcome>
+            <Welcome setIsStarted={setIsStarted} />
+          </AnimatedWelcome>
+        </CSSTransition>
+        {isStarted && (
           <>
             <Header />
-            <Main decks={decks}/>
-            <Footer total={decks[0].cards.length}/>
+            <Main decks={decks} />
+            <Footer total={decks[0].cards.length} />
           </>
-        :
-        <Welcome setIsStarted={setIsStarted}/>
-      }
+        )}
       </ViewPort>
     </AppDiv>
   );
-}
+};
 
 export default App;
